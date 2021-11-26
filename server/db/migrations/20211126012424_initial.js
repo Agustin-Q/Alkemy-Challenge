a3a4b172cf7500ae9bd1873add7f68a3187e2668
Agustin-Q <agustin.queija@gmail.com>
2021-11-25 23:04:44 -0300

    Initial setup for DB migration

--- server/db/migrations/20211126012424_initial.js
+++ server/db/migrations/20211126012424_initial.js
@@ -1,8 +1,24 @@
+const knex = require('knex');
+exports.up = async(knex) => {
+  await knex.schema.createTable('Account', (table) => {
+    table.increments().notNullable();
+    table.string('email',254).notNullable().unique();
+    table.string('name').notNullable();
+    table.string('password').notNullable();
 
-exports.up = function(knex) {
-  
+  });
+  await knex.schema.createTable('Record', (table) => {
+    table.increments().notNullable();
+    table.float('amount');
+    table.string('type');
+    table.string('category')
+    table.string('description')
+    table.timestamps(false, true);
+    table.integer('Account_id').unsigned().references('id').inTable('Record')
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async(knex) => {
+  await knex.schema.dropTable('Record');
+  await knex.schema.dropTable('Account');
 };
--- server/knexfile.js
+++ server/knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
     client: 'pg',
     version: '7.2',
     connection: {
-      host : process.env.POSTGRADE_HOST,
-      port : process.env.POSTGRADE_PORT,
-      user : process.env.POSTGRADE_USER,
-      password : process.env.POSTGRADE_PASSWORD,
-      database : process.env.POSTGRADE_DB
+      host : process.env.POSTGRES_HOST,
+      port : process.env.POSTGRES_PORT,
+      user : process.env.POSTGRES_USER,
+      password : process.env.POSTGRES_PASSWORD,
+      database : process.env.POSTGRES_DB
     },
     migrations:{
       directory: './db/migrations',
