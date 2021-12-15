1b230d27193d498274c46b4a59acab5a209a830e
Agustin-Q <agustin.queija@gmail.com>
2021-12-15 20:54:32 -0300

    Added functionality to del button to delete record from DB

--- client/src/components/Record.js
+++ client/src/components/Record.js
@@ -1,4 +1,35 @@
+import { useCallback } from "react";
+
+async function deleteRecord(id){
+  console.log(`Delete! Record with id ${id}`);
+  try {
+    const url = 'http://localhost:5000/api/v0/record';
+    const response = await fetch(url, {
+      method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('Token')}`
+      },
+      body: JSON.stringify({'id' : id}) // body data type must match "Content-Type" header
+    });
+
+    const parsedResponse = await response.json();
+    console.log(response.status);
+    if (response.status === 200){
+      console.log('Record Deleted!')
+    } else {
+      console.log('Delete not successful',parsedResponse.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function Record(props) {
+
+  const onClickDelete = useCallback(async() => {   
+    await deleteRecord(props.record.id);
+  },[props.record.id]);
   
   return (
     <div className="Record">
@@ -12,7 +43,7 @@ function Record(props) {
       </div>
       <div className="RecordButton">
       <button className="RecordButton">Edit</button>
-      <button className="RecordButton">Del</button>
+      <button className="RecordButton" onClick={onClickDelete}>Del</button>
       </div>
     </div>  
   );
