9ad08b2a3ba086e474c36b6271d1088882cc7923
Agustin-Q <agustin.queija@gmail.com>
2021-12-16 22:24:55 -0300

    Remove Knex depencency from file

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const knexConfig = require('../knexfile.js');
-const knex = require('knex')(knexConfig.development);
 const express = require('express');
 const app = express();
 const { checkTokenSetUser, checkAuth } = require('./middlewares/auth');
