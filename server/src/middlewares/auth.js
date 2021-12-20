bcc4d58611e12c8561ea788cb5190870b564a055
Agustin-Q <agustin.queija@gmail.com>
2021-12-20 19:42:02 -0300

    Removing debug logs

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -2,44 +2,35 @@ const jwt = require('jsonwebtoken');
 
 function checkTokenSetUser(req, res, next) {
   const authHeader = req.get('authorization');
-  console.log('This is the checkTokenSetUser middleware!')
   if(authHeader){
     const token = authHeader.split(' ')[1];
     if (token){
-      console.log(token);
       // verify token
       jwt.verify(token, process.env.JWT_KEY, (error, user) => {
         if (error){
-          console.log('Token verification error!')
-          // console.log(error);
           res.status(401);
           throw new Error('Auth Failed');
         } else {
           req.user = user
-          console.log(req.user);
-          console.log('token verified!');
         }
         next();
       });
     } else {
       // no token in auth header
-      console.log("No Token!");
       next();
     }
   } else {
     // No authorization header
-    console.log("No auth header!");
     next();
   }
 }
 
 function checkAuth(req, res, next) {
-  console.log('checkLoggedIn');
   if (req.user){
-    console.log('User logged!');
+    // req has user continue
     next();
   } else {
-    console.log('User NOT logged!');
+    //req does not have user 
     res.status(401);
     throw new Error('Auth Failed');
   }
