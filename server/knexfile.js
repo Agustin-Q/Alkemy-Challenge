c9d821eafbfab3b0cece207f392bc566001f6003
Agustin-Q <agustin.queija@gmail.com>
2022-01-15 19:24:59 -0300

    test knex config

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 
-if (process.env.NODE_ENV == "production"){
   module.exports = {
-    development: {
+    production: {
       client: 'pg',
       version: '7.2',
       connection: process.env.DATABASE_URL,
@@ -10,22 +9,4 @@ if (process.env.NODE_ENV == "production"){
         directory: './db/migrations',
       }
     }
-  };  
-} else {
-  module.exports = {
-    development: {
-      client: 'pg',
-      version: '7.2',
-      connection: {
-        host : process.env.POSTGRES_HOST,
-        port : process.env.POSTGRES_PORT,
-        user : process.env.POSTGRES_USER,
-        password : process.env.POSTGRES_PASSWORD,
-        database : process.env.POSTGRES_DB
-      },
-      migrations:{
-        directory: './db/migrations',
-      }
-    }
-  };  
-}
+  };
