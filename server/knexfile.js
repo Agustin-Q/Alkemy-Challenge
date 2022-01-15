1848949fbc047856a0f95beb23756b0ab414b298
Agustin-Q <agustin.queija@gmail.com>
2022-01-15 18:57:52 -0300

    added conection for heroku postgres db

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,18 +1,31 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    client: 'pg',
-    version: '7.2',
-    connection: {
-      host : process.env.POSTGRES_HOST,
-      port : process.env.POSTGRES_PORT,
-      user : process.env.POSTGRES_USER,
-      password : process.env.POSTGRES_PASSWORD,
-      database : process.env.POSTGRES_DB
-    },
-    migrations:{
-      directory: './db/migrations',
+if (process.env.NODE_ENV == "production"){
+  module.exports = {
+    development: {
+      client: 'pg',
+      version: '7.2',
+      connection: process.env.DATABASE_URL,
+      migrations:{
+        directory: './db/migrations',
+      }
     }
-  }
-};  
+  };  
+} else {
+  module.exports = {
+    development: {
+      client: 'pg',
+      version: '7.2',
+      connection: {
+        host : process.env.POSTGRES_HOST,
+        port : process.env.POSTGRES_PORT,
+        user : process.env.POSTGRES_USER,
+        password : process.env.POSTGRES_PASSWORD,
+        database : process.env.POSTGRES_DB
+      },
+      migrations:{
+        directory: './db/migrations',
+      }
+    }
+  };  
+}
